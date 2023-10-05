1cb6da5ed101cae00dcba2364d221bd2e00bb02e
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2023-10-05 13:34:19 +0300

    Добавьте файлы проекта.

---
+++ Название метода/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Название_метода
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int SetNumber(int a, int b, int c)
+        {
+            if (a < b)
+                return b;
+            else if (a > c)
+                return c;
+            else
+                return a;
+        }
+    }
+}
